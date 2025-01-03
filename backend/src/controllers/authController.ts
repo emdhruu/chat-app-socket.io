12da8cb265aba869898576b1d988bc972bf4675f
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-03 11:25:01 +0530

    Added jwt token func and created 1st user

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,12 +1,18 @@
 import User from "../models/userModel";
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils";
+
+
 
 export default class UserController {
 
     signUp = async (req: Request, res: Response): Promise<any> => {
         const { fullName, email, password } = req.body;
         try {
+            if (!fullName || !email || !password) {
+                return res.status(400).json({ message: "All fields are required" });
+            }
             if (password.length < 6) return res.status(400).json({ message: "Password must be at least 6 characters" });
 
             const user = await User.findOne({ email });
@@ -23,13 +29,24 @@ export default class UserController {
             });
 
             if (newUser) {
-                return res.status(400).json({ message: "Invalid user data" });
-            } else {
                 //generate jwt token
-                
+                const userId = newUser._id;
+                generateToken({ userId, res });
+
+                await newUser.save();
+                res.status(201).json({
+                    _id: userId,
+                    fullName: newUser.fullName,
+                    email: newUser.email,
+                    profilePic: newUser.profilePic
+                });
+            } else {
+                return res.status(400).json({ message: "Invalid user data" });
             }
 
         } catch (error) {
+            console.log("Error while signup controller", error);
+
             res.status(500).json({ message: "Internal server error" });
         }
     };
--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,1 +1,34 @@
-import jwt from "jsonwebtoken";     
+import jwt from "jsonwebtoken";
+import { Response } from "express";
+import { Types } from "mongoose";
+
+interface userProps {
+    userId: Types.ObjectId;
+    res: Response;
+}
+
+export const generateToken = ({ userId, res }: userProps): string => {
+
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) throw new Error("JWT secret key is not defined in environment variables.");
+    try {
+        const token = jwt.sign({ userId }, secret, {
+            expiresIn: "7d"
+        });
+
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV !== "development"
+        });
+
+        return token;
+
+    } catch (error) {
+        console.error("Error while generating token: ", error);
+        throw new Error("Failed to generate token");
+    }
+
+};
