185d261f40495895ca83a049ab6f9a4ee91724c3
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-04 16:24:35 +0530

    user @types in ts types

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import router from "./routes/routes";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", router);
 
