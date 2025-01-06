f6a01f4051eadcf0de42903233b51f5763fbd42f
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-06 18:11:19 +0530

    Added cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import router from "./routes/routes";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use("/api", router);
 
