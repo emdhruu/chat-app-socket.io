5654b77f75dadfd27feca00bf8dd00754f659b40
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-09 13:16:26 +0530

    remove router and just app file rendered

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,49 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage.tsx";
-import LoginPage from "./pages/LoginPage.tsx";
-import SignupPage from "./pages/SignUpPage.tsx";
-import ProfilePage from "./pages/ProfilePage.tsx";
-import SettingsPage from "./pages/SettingsPage.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "signup",
-        element: <SignupPage />,
-      },
-      {
-        path: "profile",
-        element: <ProfilePage />,
-      },
-      {
-        path: "setting",
-        element: <SettingsPage />,
-      },
-      {
-        path: "*",
-        element: <div>Page Not Found</div>,
-      },
-    ],
-  },
-]);
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
+    <Toaster position="top-center" reverseOrder={false} />
   </StrictMode>
 );
