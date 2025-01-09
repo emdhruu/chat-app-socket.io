6839bff67d180ee606986e6cd89c2258b5c16f38
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-09 13:15:57 +0530

    router added to app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import "./index.css";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -20,12 +31,45 @@ const App = () => {
       </div>
     );
 
-  return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Navbar />
+          <Outlet />
+        </>
+      ),
+      children: [
+        {
+          path: "/",
+          element: authUser ? <HomePage /> : <Navigate to="/login" />,
+        },
+        {
+          path: "login",
+          element: !authUser ? <LoginPage /> : <Navigate to="/" />,
+        },
+        {
+          path: "signup",
+          element: !authUser ? <SignupPage /> : <Navigate to="/" />,
+        },
+        {
+          path: "profile",
+          element: authUser ? <ProfilePage /> : <Navigate to="/login" />,
+        },
+        {
+          path: "setting",
+          element: <SettingsPage />,
+        },
+        {
+          path: "*",
+          element: <div>Page Not Found</div>,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
