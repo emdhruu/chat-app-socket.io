b9609cb8e463679d76f686e86886788fd69a2b63
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-12 22:52:50 +0530

    Added chatstore

---
+++ frontend/src/store/useChatStore.ts
@@ -0,0 +1,59 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios.";
+
+interface ChatState {
+    messages: any[];
+    users: any[];
+    selectedUser: any | null;
+    isUsersLoading: boolean;
+    isMessagesLoading: boolean;
+    getUsers: () => Promise<void>;
+    getMessages: (userId: string) => Promise<void>;
+    setSelectedUser: (selectedUser: any | null) => void;
+    sendMessages: (messageData: any | null) => Promise<void>;
+}
+
+
+export const useChatStore = create<ChatState>((set, get) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+
+    getUsers: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/message/users");
+            set({ users: await res.data });
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || "Failed to fetch users.");
+        } finally {
+            set({ isUsersLoading: false });
+        }
+    },
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: await res.data });
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || "Failed to fetch messages.");
+        } finally {
+            set({ isMessagesLoading: false });
+        }
+    },
+    sendMessages: async (messageData) => {
+        const { selectedUser, messages } = get();
+        try {
+            const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+            set({ messages: [...messages, res.data] });
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || "Failed to fetch messages data.");
+        }
+    },
+
+    setSelectedUser: (selectedUser) => set({ selectedUser })
+}));
