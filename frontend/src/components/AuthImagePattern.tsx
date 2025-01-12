4d2e81edb89462ce6444046e31ff34000e6fc59d
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-12 22:49:25 +0530

    changes

--- frontend/src/components/AuthImagePattern.tsx
+++ frontend/src/components/AuthImagePattern.tsx
@@ -7,7 +7,7 @@ const AuthImagePattern = ({ title, subtitle }: ImageProps) => {
   return (
     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">
-        <div className="grid grid-cols-3 gap-3 mb-8">
+        <div className="grid grid-cols-3 gap-3 m-10">
           {[...Array(9)].map((_, i) => (
             <div
               key={i}
