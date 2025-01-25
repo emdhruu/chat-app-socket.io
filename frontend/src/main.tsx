571c76330ccc1de6cab9eb76bf2631975d04cd8a
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-25 23:31:01 +0530

    Completed with chat app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { ClerkProvider } from "@clerk/clerk-react";
-
-const clerk_key: any = import.meta.env.VITE_CLERK_KEY;
-
-if (!clerk_key) {
-  throw new Error("CLERK_KEY is not defined");
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={clerk_key}>
-      <App />
-    </ClerkProvider>
+    <App />
   </StrictMode>
 );
