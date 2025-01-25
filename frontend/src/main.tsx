7e9cf79de4df46115aa2519ae3d072027bff36e1
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-25 23:01:17 +0530

    Adding clerk setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const clerk_key: any = import.meta.env.VITE_CLERK_KEY;
+
+if (!clerk_key) {
+  throw new Error("CLERK_KEY is not defined");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerk_key}>
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );
