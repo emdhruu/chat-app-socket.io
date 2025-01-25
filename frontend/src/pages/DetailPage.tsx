2b1db7b78eb24236676e3c577be97874beffd8ec
emdhruu <119742045+emdhruu@users.noreply.github.com>
2025-01-25 08:37:49 +0530

    Remove unused import and console.log

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,16 +1,11 @@
 import FriendsDetails from "../components/FriendsDetails";
 import GroupDetails from "../components/GroupDetails";
 import { useGroupState } from "../store/useGroupStore";
-import { useChatStore } from "../store/useChatStore";
 
 const DetailPage = ({ onClose }: any) => {
   const { selectedGroup } = useGroupState();
-  const {selectedUser} = useChatStore()
 
   const isGroupChat = !!selectedGroup;
-  console.log(selectedGroup);
-  console.log(selectedUser);
-  
 
   return (
     <div className="p-2.5 overflow-auto flex flex-1 flex-col">
